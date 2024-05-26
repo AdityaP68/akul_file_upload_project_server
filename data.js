77dc66e4f273c5e5927ff7999a4fc3bc550d02db
Aditya Prakash <adityaprakash6688@gmail.com>
2024-05-27 00:08:56 +0530

    img ops added

--- data.js
+++ data.js
@@ -1,12 +1,19 @@
 import fs from "node:fs/promises";
 
 const DATA_FILE_PATH = "pdfFilesData.json";
+const IMAGE_FILE_PATH = "imageFilesData.json";
 
 let pdfFiles = new Map();
+let imageFiles = new Map();
 
 export async function savedPdfFilesDataToFile() {
   const data = JSON.stringify(Array.from(pdfFiles.entries()));
   await fs.writeFile(DATA_FILE_PATH, data);
+}
+
+export async function savedImageFilesDataToFile() {
+  const data = JSON.stringify(Array.from(imageFiles.entries()));
+  await fs.writeFile(IMAGE_FILE_PATH, data);
 }
 
 export async function loadPdfFilesDataFromFile() {
@@ -24,10 +31,31 @@ export async function loadPdfFilesDataFromFile() {
   }
 }
 
+export async function loadImageFilesDataFromFile() {
+  try {
+    const data = await fs.readFile(IMAGE_FILE_PATH, "utf-8");
+    if (data.trim() === "") {
+      console.error("Nothing to load...");
+      return;
+    }
+    const entries = JSON.parse(data);
+    imageFiles = new Map(entries);
+  } catch (err) {
+    console.error("Error loading imageFiles data from file:", err);
+    imageFiles = new Map();
+  }
+}
+
 export function updatePdfFilesDataToFile() {
   savedPdfFilesDataToFile()
     .then(() => console.log("PDF files data updated successfully"))
     .catch((err) => console.error("Error updating PDF files data:", err));
 }
 
-export { pdfFiles };
+export function updateImageFilesDataToFile() {
+  savedImageFilesDataToFile()
+    .then(() => console.log("Image files data updated successfully"))
+    .catch((err) => console.error("Error updating Image files data:", err));
+}
+
+export { pdfFiles, imageFiles };
--- index.js
+++ index.js
@@ -6,13 +6,33 @@ import cors from "cors";
 import multer from "multer";
 import { v4 as uuidv4 } from "uuid";
 import fs from "node:fs/promises";
+import path from "path";
 import {
   loadPdfFilesDataFromFile,
   updatePdfFilesDataToFile,
   pdfFiles,
+  loadImageFilesDataFromFile,
+  updateImageFilesDataToFile,
+  imageFiles,
 } from "./data.js";
 
 const app = express();
+
+const UPLOADS_PDFS_DIR = "uploads/pdfs/";
+const UPLOADS_IMAGES_DIR = "uploads/images/";
+
+// Ensure directories exist
+async function ensureDirectoriesExist() {
+  try {
+    await fs.mkdir(UPLOADS_PDFS_DIR, { recursive: true });
+    await fs.mkdir(UPLOADS_IMAGES_DIR, { recursive: true });
+    console.log("Upload directories are ensured.");
+  } catch (err) {
+    console.error("Error ensuring directories:", err);
+  }
+}
+
+ensureDirectoriesExist();
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
@@ -20,10 +40,13 @@ app.use(cors());
 app.use(morgan("dev"));
 
 loadPdfFilesDataFromFile();
+loadImageFilesDataFromFile();
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    const isPdf = file.mimetype === "application/pdf";
+    const uploadPath = isPdf ? UPLOADS_PDFS_DIR : UPLOADS_IMAGES_DIR;
+    cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
@@ -32,25 +55,12 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-/**
- * This is the main route handler for the root URL ("/") of the server.
- * It sends a welcome message to the client.
- *
- * @param {Object} req - The request object.
- * @param {Object} res - The response object.
- * @param {Function} next - The next middleware function.
- */
 app.get("/", (req, res, next) => {
-  // Set the status code to 200 and send a welcome message to the client
-  res.status(200).send("Welcome to the CME pdf host server");
+  res.status(200).send("Welcome to the CME pdf and image host server");
 });
 
-/**
- * Fetches a PDF file.
- * @param {Object} req - The request object.
- * @param {Object} res - The response object.
- * @param {Function} next - The next middleware function.
- */
+// PDF Endpoints
+
 app.get("/pdf/fetch", (req, res, next) => {
   const allMetaData = Array.from(pdfFiles.values());
   res.json(allMetaData);
@@ -58,9 +68,6 @@ app.get("/pdf/fetch", (req, res, next) => {
 
 app.get("/pdf/fetch/:id", async (req, res, next) => {
   const { id } = req.params;
-
-  console.log(pdfFiles);
-
   const metadata = pdfFiles.get(id);
 
   if (!metadata) {
@@ -73,24 +80,16 @@ app.get("/pdf/fetch/:id", async (req, res, next) => {
     const data = await fs.readFile(filePath);
     res.contentType("application/pdf").send(data);
   } catch (e) {
-    console.log(e);
     return next(createError(500, "Internal Server Error"));
   }
 });
 
-/**
- * POST endpoint for creating a PDF file.
- * @param {Object} req - The request object.
- * @param {Object} res - The response object.
- * @param {Function} next - The next middleware function.
- * @returns {String} - A success message.
- */
 app.post("/pdf/create", upload.single("file"), async (req, res, next) => {
   if (!req.file) {
-    return next(createError(400, "No file Uploaded"));
+    return next(createError(400, "No file uploaded"));
   }
 
-  const { originalname, path } = req.file;
+  const { originalname, path, size } = req.file;
 
   const existingFile = Array.from(pdfFiles.values()).find(
     (file) => file.filename === originalname
@@ -101,30 +100,80 @@ app.post("/pdf/create", upload.single("file"), async (req, res, next) => {
   }
 
   const id = uuidv4();
-  const metadata = { id, filename: originalname, filepath: path };
+  const uploadDate = new Date().toISOString();
+  const metadata = {
+    id,
+    filename: originalname,
+    filepath: path,
+    size,
+    uploadDate,
+    editDate: uploadDate,
+  };
 
   pdfFiles.set(id, metadata);
   updatePdfFilesDataToFile();
 
-  console.log(pdfFiles);
-
   res.send({ success: "success", id });
 });
 
-/**
- * PATCH route to edit a PDF document.
- * @param {object} req - The request object.
- * @param {object} res - The response object.
- * @param {function} next - The next middleware function.
- */
-app.patch("/pdf/edit/:id", (req, res, next) => {
-  // Your code here
+app.patch("/pdf/edit/:id", upload.single("file"), async (req, res, next) => {
+  const { id } = req.params;
+
+  if (!pdfFiles.has(id)) {
+    return next(createError(404, "PDF not found"));
+  }
+
+  const metadata = pdfFiles.get(id);
+  const { filename, filepath, size } = metadata;
+
+  // Handle new file upload
+  if (req.file) {
+    const { originalname, path: newFilePath, size: newSize } = req.file;
+
+    // Check for filename conflict
+    const existingFile = Array.from(pdfFiles.values()).find(
+      (file) => file.filename === originalname && file.id !== id
+    );
+
+    if (existingFile) {
+      return next(createError(409, "File with the same name already exists"));
+    }
+
+    // Remove old file
+    try {
+      await fs.unlink(filepath);
+    } catch (err) {
+      return next(createError(500, "Error deleting old PDF file"));
+    }
+
+    // Update metadata with new file info
+    metadata.filename = originalname;
+    metadata.filepath = newFilePath;
+    metadata.size = newSize;
+  }
+
+  // Update metadata from request body
+  const { newFilename, newUploadDate, newEditDate } = req.body;
+  
+  if (newFilename) metadata.filename = newFilename;
+  if (newUploadDate) metadata.uploadDate = newUploadDate;
+  if (newEditDate) metadata.editDate = newEditDate;
+
+  // Always update edit date to current date if not provided
+  metadata.editDate = new Date().toISOString();
+
+  // Save changes
+  pdfFiles.set(id, metadata);
+  updatePdfFilesDataToFile();
+
+  res.send({ success: "PDF updated successfully", id });
 });
+
+
 
 app.delete("/pdf/delete/:id", async (req, res, next) => {
   const { id } = req.params;
 
-  // Check if the PDF file with the provided ID exists
   if (!pdfFiles.has(id)) {
     return next(createError(404, "PDF not found"));
   }
@@ -139,35 +188,149 @@ app.delete("/pdf/delete/:id", async (req, res, next) => {
 
     res.send({ success: true, message: "PDF file deleted successfully" });
   } catch (err) {
-    console.error("Error deleting file:", err);
     next(createError(500, "Error deleting PDF file"));
   }
 });
 
-// Middleware to handle 404 errors
+// Image Endpoints
+
+app.get("/image/fetch", (req, res, next) => {
+  const allMetaData = Array.from(imageFiles.values());
+  res.json(allMetaData);
+});
+
+app.get("/image/fetch/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const metadata = imageFiles.get(id);
+
+  if (!metadata) {
+    return next(createError(404, "Image not found"));
+  }
+
+  const filePath = metadata.filepath;
+
+  try {
+    const data = await fs.readFile(filePath);
+    res.contentType("image/jpeg").send(data); // Change the content type as needed
+  } catch (e) {
+    return next(createError(500, "Internal Server Error"));
+  }
+});
+
+app.post("/image/create", upload.single("file"), async (req, res, next) => {
+  if (!req.file) {
+    return next(createError(400, "No file uploaded"));
+  }
+
+  const { originalname, path } = req.file;
+
+  const existingFile = Array.from(imageFiles.values()).find(
+    (file) => file.filename === originalname
+  );
+
+  if (existingFile) {
+    return next(createError(409, "File with the same name already exists"));
+  }
+
+  const id = uuidv4();
+  const uploadDate = new Date().toISOString();
+  const metadata = {
+    id,
+    filename: originalname,
+    filepath: path,
+    size: req.file.size,
+    uploadDate,
+    editDate: uploadDate,
+  };
+
+  imageFiles.set(id, metadata);
+  updateImageFilesDataToFile();
+
+  res.send({ success: "success", id });
+});
+
+app.patch("/image/edit/:id", upload.single("file"), async (req, res, next) => {
+  const { id } = req.params;
+
+  if (!imageFiles.has(id)) {
+    return next(createError(404, "Image not found"));
+  }
+
+  const metadata = imageFiles.get(id);
+  const { filename, filepath, size } = metadata;
+
+  // Handle new file upload
+  if (req.file) {
+    const { originalname, path: newFilePath, size: newSize } = req.file;
+
+    const existingFile = Array.from(imageFiles.values()).find(
+      (file) => file.filename === originalname && file.id !== id
+    );
+
+    if (existingFile) {
+      return next(createError(409, "File with the same name already exists"));
+    }
+
+    try {
+      await fs.unlink(filepath);
+    } catch (err) {
+      return next(createError(500, "Error deleting old image file"));
+    }
+
+    metadata.filename = originalname;
+    metadata.filepath = newFilePath;
+    metadata.size = newSize;
+  }
+
+  // Update metadata from request body
+  const { newFilename, newUploadDate } = req.body;
+
+  if (newFilename) metadata.filename = newFilename;
+  if (newUploadDate) metadata.uploadDate = newUploadDate;
+
+  // Always update edit date to current date
+  metadata.editDate = new Date().toISOString();
+
+  // Save changes
+  imageFiles.set(id, metadata);
+  updateImageFilesDataToFile();
+
+  res.send({ success: "Image updated successfully", id });
+});
+
+
+
+app.delete("/image/delete/:id", async (req, res, next) => {
+  const { id } = req.params;
+
+  if (!imageFiles.has(id)) {
+    return next(createError(404, "Image not found"));
+  }
+
+  const metadata = imageFiles.get(id);
+  const filePath = metadata.filepath;
+
+  try {
+    await fs.unlink(filePath);
+    imageFiles.delete(id);
+    updateImageFilesDataToFile();
+
+    res.send({ success: true, message: "Image file deleted successfully" });
+  } catch (err) {
+    next(createError(500, "Error deleting image file"));
+  }
+});
+
 app.use("*", (req, res, next) => {
   next(createError(404, "Resource Not Found"));
 });
 
-/**
- * Middleware function to handle errors in the Express app.
- * @param {Error} err - The error object.
- * @param {Object} req - The request object.
- * @param {Object} res - The response object.
- * @param {Function} next - The next middleware function.
- */
 app.use((err, req, res, next) => {
-  /**
-   * Create an error object with status and message properties.
-   * If the error object does not have a status property, default to 500.
-   * If the error object does not have a message property, default to "Internal Server Error".
-   */
   const error = {
     status: err.status || 500,
     message: err.message || "Internal Server Error",
   };
 
-  // Set the response status code to the error status or default to 500.
   res.status(err.status || 500).send(error);
 });
 
